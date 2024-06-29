bdb72d79f3d74483ae6739bf977dc610b98d38fb
Rustamjonov Davron <davronalirustamjonov@gmail.com>
2024-06-29 15:14:42 +0500

    Add

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -9,7 +9,7 @@ const Menu = () => {
         <ul className="menu__list">
           <li className="menu__item">
             <h3 className="menu__title">Contact Us</h3>
-            <p className="menu__text">In dignissim euismod pretium amet enim a eu nam ut urna accumsan pellentesque lacus duis pharetra eutortor.</p>
+            <p className="menu__four-text">In dignissim euismod pretium amet enim a eu nam ut urna accumsan pellentesque lacus duis pharetra eutortor.</p>
             <div className="menu__contact">
               <img className="telephone-pic" src={Telephone} alt="Telephone Icon" />
               <div className="menu__contact-info">
@@ -21,14 +21,15 @@ const Menu = () => {
           <hr/>
           <li className="menu__item">
             <h3 className="menu__next-title">Not convinced yet?</h3>
-            <p className="">Massa bibendum consectetur maurisid gravida purus, dolor dui amet morbi non nunc urna purus diam.</p>
+            <p className="menu__four-text">Massa bibendum consectetur maurisid gravida purus, dolor dui amet morbi non nunc urna purus diam.</p>
             <button className="menu__button">Browse our packages</button>
           </li>
         </ul>
 
     
-        <ul className="menu__list">
           <form className="menu__form" action="#">
+            
+        <ul  className="menu__next-list">
             <li>
               <p className="menu__text">Full name</p>
               <input className="menu__input" type="text" />
@@ -51,17 +52,18 @@ const Menu = () => {
             </li>
             <li>
               <p className="menu__text">Day of service</p>
-              <input className="menu__input" type="date" />
+              <input className="menu__input" type="text" />
             </li>
-            <li>
+            <li className="menu__next-item">
               <p className="menu__text">Add a note</p>
               <input className="menu__next-input" type="text" />
             </li>
             <li>
-              <button className="menu__button">Submit</button>
+              <button className="menu__button menu__btn">Submit message</button>
             </li>
+            </ul>
           </form>
-        </ul>
+        
       </div>
     </div>
   );
