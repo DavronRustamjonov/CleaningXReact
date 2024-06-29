4a1db48f4339e036efed17372beffca1f7b8ead9
Rustamjonov Davron <davronalirustamjonov@gmail.com>
2024-06-29 13:36:35 +0500

    Add

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -11,17 +11,17 @@ const Menu = () => {
             <h3 className="menu__title">Contact Us</h3>
             <p className="menu__text">In dignissim euismod pretium amet enim a eu nam ut urna accumsan pellentesque lacus duis pharetra eutortor.</p>
             <div className="menu__contact">
-              <img src={Telephone} alt="Telephone Icon" />
+              <img className="telephone-pic" src={Telephone} alt="Telephone Icon" />
               <div className="menu__contact-info">
-                <p>Call us now</p>
-                <p>(414) 567 - 2109</p>
+                <p className="menu__frist-text">Call us now</p>
+                <p className="menu__next-text">(414) 567 - 2109</p>
               </div>
             </div>
           </li>
           <hr/>
           <li className="menu__item">
-            <h3>Not convinced yet?</h3>
-            <p>Massa bibendum consectetur maurisid gravida purus, dolor dui amet morbi non nunc urna purus diam.</p>
+            <h3 className="menu__next-title">Not convinced yet?</h3>
+            <p className="">Massa bibendum consectetur maurisid gravida purus, dolor dui amet morbi non nunc urna purus diam.</p>
             <button className="menu__button">Browse our packages</button>
           </li>
         </ul>
@@ -30,32 +30,32 @@ const Menu = () => {
         <ul className="menu__list">
           <form className="menu__form" action="#">
             <li>
-              <p>Full name</p>
-              <input type="text" />
+              <p className="menu__text">Full name</p>
+              <input className="menu__input" type="text" />
             </li>
             <li>
-              <p>Phone number</p>
-              <input type="tel" />
+              <p className="menu__text">Phone number</p>
+              <input className="menu__input" type="tel" />
             </li>
             <li>
-              <p>Address</p>
-              <input type="text" />
+              <p className="menu__text">Address</p>
+              <input className="menu__input" type="text" />
             </li>
             <li>
-              <p>Email</p>
-              <input type="email" />
+              <p className="menu__text">Email</p>
+              <input className="menu__input" type="email" />
             </li>
             <li>
-              <p>Requested service</p>
-              <input type="text" />
+              <p className="menu__text">Requested service</p>
+              <input className="menu__input" type="text" />
             </li>
             <li>
-              <p>Day of service</p>
-              <input type="date" />
+              <p className="menu__text">Day of service</p>
+              <input className="menu__input" type="date" />
             </li>
             <li>
-              <p>Add a note</p>
-              <input type="text" />
+              <p className="menu__text">Add a note</p>
+              <input className="menu__next-input" type="text" />
             </li>
             <li>
               <button className="menu__button">Submit</button>
